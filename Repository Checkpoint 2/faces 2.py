abbb544aae12294e51bc762b767ba73d0902c558
LunaRobles2 <1lunabisakura@gmail.com>
2025-08-19 16:22:20 +0000

    face2

--- Repository Checkpoint 2/faces 2.py
+++ Repository Checkpoint 2/faces 2.py
@@ -2,8 +2,8 @@ def main():
     emoji = input("Write a mesage with emoticon: ")
     convert (emoji)
 
-def convert (message)
-    message = message.replace (":)" , "😊").replace(":(" , "😟")")
+def convert (message):
+    message = message.replace (":)" , "😊").replace(":(" , "😟")
     print (message)
 
 main()

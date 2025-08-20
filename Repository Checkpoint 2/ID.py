9e28cff125b76d15716e9194969b73a78dcae6ab
LunaRobles2 <1lunabisakura@gmail.com>
2025-08-20 20:20:32 +0000

    33 error

---
+++ Repository Checkpoint 2/ID.py
@@ -0,0 +1,35 @@
+def main():
+	print('')
+	print('Please enter the following information to create your ID Card.')
+	input('Press enter to continue.')
+
+    name = input("Your name: ")
+    Last_Name = input("Your Last Name: ")
+	Email_Address = input("Email Adress: ")
+	Phone_Number = input("Phone Number: ")
+	Student_ID_Number = input("Student ID Number (This can be a mix of letters and numbers, like 'S12345'): ")
+	FPT_Class= input("FPT Class (e.g., nformatica, Accounting"): ")
+	Expected_Graduation_Year = input("Expected Graduation Year (e.g., 2026): ")
+	Favorite_Subject = input("Favorite Subject: ")
+	Extracurricular_Activities = input("Extracurricular Activities (Yes or No): ")
+	
+	print ("Your ID Card is:")
+	line = "-" * 45
+	print(line)
+    print(Last_Name.upper,name)
+	print(student)
+    print(Student_ID_Number)
+    
+	CALAHORRA, Aaron
+Teacher
+ID: 13789782478917
+
+aaroncalahorra2@churchofjesuschrist.org
+******9980
+
+FPT Class: Informatica       Favorite Subject: Info
+Expected Graduation: 2026    Years Left: 1
+  
+Extracurricular Activities: Yes
+
+main()

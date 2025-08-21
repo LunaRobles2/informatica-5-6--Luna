739d58bc33ddd6460037e30dcffe07de74948f16
LunaRobles2 <1lunabisakura@gmail.com>
2025-08-21 20:20:05 +0000

    if else

---
+++ Repository Checkpoint 2/if-exercises.py
@@ -0,0 +1,14 @@
+def main():
+    x= int (input ("type an integer number: "))
+    convert(x)
+
+def convert(x):
+    negative= (x*(-1))
+    positive= (x)
+    x>0:
+        print("The absolute value is", (positive))
+    else:
+        if x<0:
+            print("The absolute value is", (negative))
+
+main()

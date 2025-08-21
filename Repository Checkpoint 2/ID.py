072155bf3fd9ffb1d720358ace89bb4689a5d874
LunaRobles2 <1lunabisakura@gmail.com>
2025-08-21 17:13:09 +0000

    ID

--- Repository Checkpoint 2/ID.py
+++ Repository Checkpoint 2/ID.py
@@ -8,7 +8,7 @@ def main():
 	Email_Address = input("Email Adress: ")
 	Phone_Number = input("Phone Number: ")
 	Student_ID_Number = input("Student ID Number (This can be a mix of letters and numbers, like 'S12345'): ")
-	FPT_Class= input("FPT Class (e.g., nformatica, Accounting"): ")
+	FPT_Class= input("FPT Class (e.g., nformatica, Accounting): ")
 	Expected_Graduation_Year = input("Expected Graduation Year (e.g., 2026): ")
 	Favorite_Subject = input("Favorite Subject: ")
 	Extracurricular_Activities = input("Extracurricular Activities (Yes or No): ")
@@ -17,22 +17,14 @@ def main():
 	line = "-" * 45
 	print(line)
 	print(Last_Name.upper,name)
-	print(student)
 	print(Student_ID_Number)
-	Email_Address
-	Phone_Number
-	
+	print(Email_Address)
+	print(Phone_Number)
+	print(FPT_Class)
+	print(Expected_Graduation_Year)
+	print(line)
+	print(Favorite_Subject)
+	print(Extracurricular_Activities)
 
-	CALAHORRA, Aaron
-Teacher
-ID: 13789782478917
-
-aaroncalahorra2@churchofjesuschrist.org
-******9980
-
-FPT Class: Informatica       Favorite Subject: Info
-Expected Graduation: 2026    Years Left: 1
-  
-Extracurricular Activities: Yes
 
 main()

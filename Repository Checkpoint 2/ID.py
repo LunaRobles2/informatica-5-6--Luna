1ac47eb878ec0d5131e7ad284c26af07d48f14d6
LunaRobles2 <1lunabisakura@gmail.com>
2025-08-21 16:10:41 +0000

    25error

--- Repository Checkpoint 2/ID.py
+++ Repository Checkpoint 2/ID.py
@@ -2,9 +2,9 @@ def main():
 	print('')
 	print('Please enter the following information to create your ID Card.')
 	input('Press enter to continue.')
-
-    name = input("Your name: ")
-    Last_Name = input("Your Last Name: ")
+	bgbg
+	name = input("Your name: ")
+	Last_Name = input("Your Last Name: ")
 	Email_Address = input("Email Adress: ")
 	Phone_Number = input("Phone Number: ")
 	Student_ID_Number = input("Student ID Number (This can be a mix of letters and numbers, like 'S12345'): ")
@@ -12,14 +12,14 @@ def main():
 	Expected_Graduation_Year = input("Expected Graduation Year (e.g., 2026): ")
 	Favorite_Subject = input("Favorite Subject: ")
 	Extracurricular_Activities = input("Extracurricular Activities (Yes or No): ")
-	
+
 	print ("Your ID Card is:")
 	line = "-" * 45
 	print(line)
-    print(Last_Name.upper,name)
+	print(Last_Name.upper,name)
 	print(student)
-    print(Student_ID_Number)
-    
+	print(Student_ID_Number)
+
 	CALAHORRA, Aaron
 Teacher
 ID: 13789782478917

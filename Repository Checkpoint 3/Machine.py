c057c4a1eac51b976184d027333ad18e09b3c149
LunaRobles2 <1lunabisakura@gmail.com>
2025-08-28 17:29:04 +0000

    coke

--- Repository Checkpoint 3/Machine.py
+++ Repository Checkpoint 3/Machine.py
@@ -3,8 +3,15 @@ def main():
     name = input("Please type your name: ")
     amount = 0
     while amount < 50:
-        coin = int(input("Incert a coin: "))
-        if coin == 25 or coin = 10 or coin = 5:
-            print 
+        coin = int(input("Please incert a coin: "))
+        if coin == 25 or coin == 10 or coin == 5:
+            amount = amount + coin
+            print (amount)
+        else:
+            print ("we just accept coins of $25 $10 or $5: ")
+
+    change = amount - 50
+    print ('Here is a Coke for', name)
+    print ('Here your change', change)
 
 main()

1f9452fd41972d6e52fd8e2b8612e168912aa9c3
LunaRobles2 <1lunabisakura@gmail.com>
2025-08-29 17:32:43 +0000

    month

---
+++ Repository Checkpoint 3/real_life.py
@@ -0,0 +1,24 @@
+def main():
+
+    print ('This is a month To Do list to organize your time')
+    name = input("Please type your name: ")
+    month = input("Please type the month: ")
+    day = 0
+    while next == next:
+        print (month)
+        print (day + 1)
+        while ok == ok:
+            list = input("Something to do today: ")
+            ok = input("Did you want to add more?: ")
+            if ok == "no":
+                print('Today you have to remember to')
+
+        print ('Pray')
+        print ('Read scriptures')
+        print ('Be ready to sleep at 10pm')
+        print ('go to school')
+        print ('And also dont forget to', list)
+    next = input(name,"Please type -next- to coninue or -stop- to end the program: ")
+
+
+main()

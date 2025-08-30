8ef4b0fb1af4fd49abf620a3cdba7ef4e89b9165
LunaRobles2 <1lunabisakura@gmail.com>
2025-08-30 01:07:10 +0000

    luna project

--- Repository Checkpoint 3/real_life.py
+++ Repository Checkpoint 3/real_life.py
@@ -1,24 +1,37 @@
 def main():
-
     print ('This is a month To Do list to organize your time')
-    name = input("Please type your name: ")
-    month = input("Please type the month: ")
-    day = 0
-    while next == next:
-        print (month)
-        print (day + 1)
-        while ok == ok:
-            list = input("Something to do today: ")
-            ok = input("Did you want to add more?: ")
-            if ok == "no":
-                print('Today you have to remember to')
+    while True:
+        next = int(input("Please type -1- to coninue or -2- to end the program: "))
+        
+        if next == 1:
+            name = input("Please type your name: ")
+            a = "Pray"
+            b = "Read scriptures"
+            c = "Be ready to sleep at 10pm"
+            d = "go to school"
+            line = "-" * 45
 
-        print ('Pray')
-        print ('Read scriptures')
-        print ('Be ready to sleep at 10pm')
-        print ('go to school')
-        print ('And also dont forget to', list)
-    next = input(name,"Please type -next- to coninue or -stop- to end the program: ")
+            date = input("Please type the date: ")
+            list = input("something importat for today is:")
+            ok = int(input("Did you want to add more? if yes put -3- if not clic -4-: "))
+            if ok == 3:
+                other = input("add here: ")
+            else:
+                other = " "
+                print("okay")
 
+            print (line)
+            print ('Today is:', date)
+            print (name,'Today you need to remember to...')
+            print (a)
+            print (b)
+            print (c)                
+            print (d)
+            print (list)
+            print (other)
+            print (line)
+
+        else:
+            break
 
 main()

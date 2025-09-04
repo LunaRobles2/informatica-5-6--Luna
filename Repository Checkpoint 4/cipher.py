222a2a687fd4b7439b1d7263471fe0dec4a17c68
LunaRobles2 <1lunabisakura@gmail.com>
2025-09-04 16:36:05 +0000

    tardado

--- Repository Checkpoint 4/cipher.py
+++ Repository Checkpoint 4/cipher.py
@@ -5,14 +5,18 @@ def main():
 def encode_message(text):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     cipher = "zyxwvutsrqponmlkjihgfedcba"
-    new_message = " "
-    i=0
-
-    print ("-" * len(message))
-
+    new_message = ""
+    i = 0
 
     while i < len(text):
         char = text[i]
         
+        if char in alphabet:
+            cipher_index = alphabet.find(char)
+            new_message += cipher [cipher_index]
+        else:
+            new_message += char
+        i += 1
+    print ('encoded message: ' + new_message)
 
-main()                                                                                                         
+main()                                                                                                          

c94a453032d42414a98b128923bd90adb25747f7
LunaRobles2 <1lunabisakura@gmail.com>
2025-09-04 17:31:41 +0000

    si

---
+++ Repository Checkpoint 4/multitable.py
@@ -0,0 +1,22 @@
+def main():
+    num = int(input("enter the number of table you want: "))
+    table = 1
+    while table < 11:
+        print(f"{num} x {table} = {num * table}")
+        table += 1
+
+main()
+
+def main():
+    num = int(input("enter the number of table you want: "))
+    table = 1
+    while table < (num + 1):
+        print(f"{num} x {table} = {num * table}")
+        table += 1
+
+    while table < (num + 1):
+        print(f"{table} x {other} = {table * other}")
+        table += 1
+        other += 1
+
+main()

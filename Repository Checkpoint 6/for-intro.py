7973003c1f25d8d9971d1e3b53116b0d5755b89f
LunaRobles2 <1lunabisakura@gmail.com>
2025-09-17 20:20:02 +0000

    objectives

---
+++ Repository Checkpoint 6/for-intro.py
@@ -0,0 +1,11 @@
+docrine = ["Faith", "Repentance", "Baptism", "Holy Ghost", "Endure to the end"]
+
+# i = 0
+# while i < len(doctrine):
+#    print (docrine[i])
+#    i += 1
+
+for principle in docrine:
+    print(principle)
+
+school_objective

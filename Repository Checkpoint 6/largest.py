f4c2978fbaa3e8906f3a82c25b8355c3917aae24
LunaRobles2 <1lunabisakura@gmail.com>
2025-09-19 17:32:29 +0000

    num

--- Repository Checkpoint 6/largest.py
+++ Repository Checkpoint 6/largest.py
@@ -1,2 +1,19 @@
 def main():
-    length = int(input("Enter the length of a list: "))
+    list_length = int(input("Enter the length of a list: "))
+    num_list = []
+
+    for number in range(list_length):
+        list_element = int(input("Enter element: "))
+        num_list.append (list_element)
+        file= open("largest.txt","a")
+        file.write(f"{list_element}\n")
+        file.close()
+
+    print (num_list) 
+
+    large_num = max(num_list)
+    print ("The largest number is:",large_num)
+
+
+
+main()

e31738c7bea7841cc48cfb2a4381da7b25bb568c
LunaRobles2 <1lunabisakura@gmail.com>
2025-09-30 16:41:00 +0000

    sip

---
+++ Repository Checkpoint 8/character-count.py
@@ -0,0 +1,12 @@
+def character_counter (message, dictionary):
+    for character in message:
+        dictionary.setdefault(character, 0)
+        dictionary [character] += 1
+
+    print(dictionary)
+
+message = input("Write a message: ")
+dictionary = {}
+character_counter(message, dictionary)
+
+

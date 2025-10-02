7f6848c45c8d901895ec94aa52e3bdcfcbf5c3f7
LunaRobles2 <1lunabisakura@gmail.com>
2025-10-02 15:46:02 +0000

    counter

--- Repository Checkpoint 8/character-count.py
+++ Repository Checkpoint 8/character-count.py
@@ -7,13 +7,22 @@ def character_counter (message, dictionary):
 
     print(sum(dictionary.values()))
 
+    print("-" * 45)
     #Alternative 1
+    print("#Alternative 1")
     vlalues_list = list(dictionary.values())
     print(vlalues_list)
     largest_number_index = vlalues_list.index(max(vlalues_list))
     repeted_character = list(dictionary.keys())[largest_number_index]
     print(f"The most repeted character is: {repeted_character} , repeating {dictionary[repeted_character]} times")
 
+    print("-" * 45)
+    #Alternative 2
+    print("#Alternative 2")
+    largest_number2 = max(dictionary, key = dictionary.get)
+    print(f"The most repeted character is: {largest_number2} , repeating {dictionary[repeted_character]} times")
+
 message = input("Write a message: ")
+print("-" * 45)
 dictionary = {}
 character_counter(message, dictionary)

e56897c8b0c12d1e8635645fb87d6323f2c23eba
LunaRobles2 <1lunabisakura@gmail.com>
2025-10-03 17:34:21 +0000

    sc

---
+++ Repository Checkpoint 8/scrabble.py
@@ -0,0 +1,15 @@
+print ("⚫⚪⚫⚪⚫   SCRABBLE   ⚫⚪⚫⚪⚫")
+print ("To play the game, each player is given 13 letters, which they must \n rearrange to create words. Different letters have different point \n values, since it's easier to create words with some letters than others.")
+print("-" * 45)
+
+import random
+import string
+
+dictionary = {}
+A = 0
+while A < 13:
+    num1 = random.choice(string.ascii_uppercase) 
+    print (num1)
+    A += 1
+    num1.append
+print(list)

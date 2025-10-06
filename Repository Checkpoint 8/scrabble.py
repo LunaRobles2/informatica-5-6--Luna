429b2f1a76b290c7a33426a30e30b3f1cbb63892
LunaRobles2 <1lunabisakura@gmail.com>
2025-10-06 20:17:55 +0000

    letras

--- Repository Checkpoint 8/scrabble.py
+++ Repository Checkpoint 8/scrabble.py
@@ -4,12 +4,19 @@ print("-" * 45)
 
 import random
 import string
-
-dictionary = {}
-A = 0
-while A < 13:
-    num1 = random.choice(string.ascii_uppercase) 
-    print (num1)
-    A += 1
-    num1.append
-print(list)
+alphabet = {
+    "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1,
+    "D": 2, "G": 2,
+    "B": 3, "C": 3, "M": 3, "P": 3,
+    "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4, 
+    "K": 4, 
+    "J": 8, "X": 8, 
+    "Q": 10, "Z": 10, 
+}
+user_letters = []
+i = 0
+while i < 13:
+    i += 1
+    num = random.choice(string.ascii_uppercase) 
+    user_letters.append(num)
+print(user_letters)

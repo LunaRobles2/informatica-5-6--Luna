94a3c85ff19a890eccec6cb0017ac9870691a993
LunaRobles2 <1lunabisakura@gmail.com>
2025-10-21 04:15:26 +0000

    m

--- Repository Checkpoint 8/scrabble.py
+++ Repository Checkpoint 8/scrabble.py
@@ -1,41 +1,71 @@
-print ("⚫⚪⚫⚪⚫   SCRABBLE   ⚫⚪⚫⚪⚫")
-print ("To play the game, each player is given 13 letters, which they must \n rearrange to create words. Different letters have different point \n values, since it's easier to create words with some letters than others.")
-print("-" * 45)
+def main():
+    print("⚫⚪⚫⚪⚫   SCRABBLE   ⚫⚪⚫⚪⚫")
+    print("To play the game, each player is given 13 letters, which they must")
+    print("rearrange to create words. Different letters have different point")
+    print("values, since it's easier to create words with some letters than others.")
+    print("-" * 45)
 
-import random
-import string
-alphabet = {
-    "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1,
-    "D": 2, "G": 2,
-    "B": 3, "C": 3, "M": 3, "P": 3,
-    "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4, 
-    "K": 4, 
-    "J": 8, "X": 8, 
-    "Q": 10, "Z": 10, 
-}
-user_letters = []
-i = 0
-while i < 13: 
-    user_letters.append(list(alphabet.keys())[random.randint(0,25)])
-    i += 1
-print(user_letters)
+    import random
+    alphabet = {
+        'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+        'D': 2, 'G': 2,
+        'B': 3, 'C': 3, 'M': 3, 'P': 3,
+        'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+        'K': 5,
+        'J': 8, 'X': 8,
+        'Q': 10, 'Z': 10
+    }
 
-word = input("Enter a word with these letters: ")
-o = 0
-while o < len(word):
-    if word.upper()[o] in user_letters:
-        print(word[o])
-        o += 1
-    else:
-        print("Wrong")
-        o += 1
+    # Picks 13 random letters from the alphabet
+    user_letters = []
+    i = 0
+    while i < 13:
+        user_letters.append(list(alphabet.keys())[random.randint(0,25)])
+        i += 1
+    print(user_letters)
 
-with open("scrabble-words.txt", "r") as file:
-    lines = file.readline()
-dicwords = []
-for line in lines:
-    dicwords.append(line.replace("\n",""))
-if word in dicwords:
-    print("it's okay")
-else: 
-    print("Not okay")
+    #Ask the usser for a word with the given letters
+    word = input("Enter a word with these letters: ").upper()
+
+    # Checks if the word exist and counts
+    score = 0
+    while True:
+        while True:        
+            # Check if the word is in the dictionary                                                
+            with open("scrabble-words.txt" ,"r") as file:           
+                lines = file.readlines()                            
+            dictwords = []
+            for line in lines:
+                dictwords.append(line.replace("\n",""))
+            
+            # Told you if the word is in the dictionary
+            if word.lower() in dictwords:                          
+                print("Valid")
+                break
+
+            # Told you if the word is not in the dictionary
+            else: 
+                word = input("Not valid, try again: ").upper()
+                if word == "":
+                    break
+
+         # Checks if the input is ENTER
+        if word != "": 
+
+            # Removes the letters the user entered
+            for letter in word:                                     
+                user_letters.pop(user_letters.index(letter))        
+            
+            # Adds the value of every letter
+            for value in word:                                      
+                score += alphabet[value]
+            
+            print(f"Your total score is: {score}")
+            print(f"Remaining letters: \n{user_letters}")
+            word = input("Enter a word with the remaining letters, press ENTER to stop: ").upper()
+            if word == "":
+                break
+        else: break
+    print(f"Thank you for playing! Your final score is {score}")
+
+main()

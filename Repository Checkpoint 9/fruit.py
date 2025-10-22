55fe3da2ddf996d9b12810f8cb91dcfeada466ff
LunaRobles2 <1lunabisakura@gmail.com>
2025-10-22 19:50:35 +0000

    list

---
+++ Repository Checkpoint 9/fruit.py
@@ -0,0 +1,12 @@
+word_list = [
+  "banana", "milk", "soda", "cheese", "sourmilk", "juice", "sausage",
+  "tomato", "cucumber", "butter", "margarine", "cheese", "sausage",
+  "soda", "sourmilk", "sourmilk", "butter", "soda", "chocolate"
+]
+
+# 2. Create a function called `count` to that prints a dictionary using the words 
+# in the list as keys of the dictionary, and the number of times the word has a
+# ppeared as the value mapped to each key.
+# 3. Insert a breakpoint in the line of the first loop statement that you type.
+# 4. Run the program in a debugging session.
+# def count():

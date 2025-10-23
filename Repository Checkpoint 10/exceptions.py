a3464562eab01c8ab5d0be8e53b186d3c4a119cf
LunaRobles2 <1lunabisakura@gmail.com>
2025-10-23 17:23:20 +0000

    error

---
+++ Repository Checkpoint 10/exceptions.py
@@ -0,0 +1,41 @@
+#Sintax Error
+#print("Hello world)
+
+#Value Error
+#try:
+#    x = int(input("What is x? "))
+#    x = print(f"x is equal to {x}")
+#except ValueError:
+#    print("x is not a number")
+
+#
+# def spam(dibide_by):
+#     try:
+#         return 42 / dibide_by
+#     except ZeroDivisionError:
+#         print("Invalid Argument")
+
+# print(spam(2))
+# print(spam(12))
+# print(spam(0))
+# print(spam(1))
+
+# #Guess
+# def spam(dibide_by):
+#     if dibide_by>0:
+#         return 42 / dibide_by
+# print(spam(2))
+# print(spam(12))
+# print(spam(0))
+# print(spam(1))
+
+#
+# while True:
+#     try:
+#         x = int(input("What is x? "))
+#     except ValueError:
+#         print("x is not a number")
+#     else:
+#         break
+
+# x = print(f"x is equal to {x}")

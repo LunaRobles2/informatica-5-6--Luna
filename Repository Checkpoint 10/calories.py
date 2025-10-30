9965547a604fe5c5b5b1439287cccac68e5f9cc3
LunaRobles2 <1lunabisakura@gmail.com>
2025-10-30 20:20:53 +0000

    list food

---
+++ Repository Checkpoint 10/calories.py
@@ -0,0 +1,30 @@
+def main():
+    print("🥛 🍉 " * 10)
+    print("Count your calories!!!")
+    dic = {
+    "Whole milk" : "73",
+    "Soymilk, light" : "30",
+    "Yogurt Plain" : "68.5",
+    "Yogurt Sour Cream" : "16",
+    "egg" : "75",
+    "egg white" : "16",
+    "Cream Cheese" : "17",
+    "Butter, regular, salted " : "36",
+    "Almonds" : "170",
+    "Peanuts" : "166",
+    "Black beans" : "113",
+    "Pinto beans" : "122",
+    "Chicken Wing" : "80",
+    "Pork" : "178",
+    "Carrots" : "13",
+    "Lettuce" : "2",
+    "Watermellon" : "11",
+    "Banana" : "33"
+    }
+
+    x = input("Write a food item from the list: ")
+    y = input("Write another food item from the list: ")
+    print(dic)
+
+    
+main()

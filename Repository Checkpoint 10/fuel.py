e6807dd11da7f548953b96cbe4f13dd5209dc7fc
LunaRobles2 <1lunabisakura@gmail.com>
2025-10-30 16:37:15 +0000

    c

--- Repository Checkpoint 10/fuel.py
+++ Repository Checkpoint 10/fuel.py
@@ -1,28 +1,41 @@
 def main():
-    print("Fuel gauges indicate, often with fractions, just how much fuel is in a tank.")
+    print(" ⏱️   ⚫ " * 10)
+    print("Fuel gauges indicate, often with fractions, just how much fuel is in a tank.⛽")
     print("For instance 1/4 indicates that a tank is 25%, full, 1/2 indicates that a tank")
-    print("is 50%, full, and 3/4 indicates that a tank is 75%, full.")
+    print("is 50%🚩, full, and 3/4 indicates that a tank is 75%, full🚩.")
     print(" ")
-    print("This program checks how much fuel is in a tank, to do this you must create a fraction ")
+    print("This program checks how much fuel is in a tank🚗, to do this you must create a fraction ")
     print("without integers forming X/Y, where in each of X and Y is a positive integer")
-    print("so that the program gives it to you in percentage rounded to the nearest integer.")
+    print("so that the program gives it to you in percentage rounded to the nearest integer⏱️.")
 
-    x = read_input("Please type in a number for your X: ")
-    y = read_input("Please type in a number for your Y: ")
-    per = (x/y) * 100
-    print(x, "/", y, " indicates that a tank is", per, "%, full")
+    x = input("⛽Please type the fuel level as a fraction: ").split("/")
+    print("-" * 45)
+    xx = x[0]
+    yy = x[1]
+    read_input(xx, yy)
+    #print(xx, "/", yy, " indicates that a tank is", , "%, full")
 
 def read_input(xx, yy):
     while True:
-        try: 
-            n = int(input(xx))
-            m = int(input(yy))
-            if xx < yy: 
-                return xx
-            else: 
+        try:
+            if int(xx) < int(yy):
+                per = round(int(xx)/int(yy) * 100)
+                if per == 1 or per < 1:
+                    print("🔴E🔴")
+                    print(" ")
+                    break
+                elif per == 99 or per > 99:
+                    print("🟢F🟢")
+                    print(" ")
+                    break
+                else:
+                    print("⏩⏩⏩", xx, "/", yy, " indicates that a tank ⛽ is", per, "%, full⛽")
+                    print(" ")
+                    break
+            else:
                 print("It is not necessary for Y to be", yy)
-        except ZeroDivisionError:
+        except ZeroDivisionError or ValueError or TypeError:
             print("It is not necessary for Y to be", yy)
-        except ValueError:
-            print("It is not necessary for Y to be", yy)
-        
+        except IndexError:
+            print("Sorry but you have to type a number, a dash and another number")
+main()

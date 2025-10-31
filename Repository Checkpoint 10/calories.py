e8e873ba6cd0fe1a1e8bbbebc2bf0c35920a1772
LunaRobles2 <1lunabisakura@gmail.com>
2025-10-31 17:31:55 +0000

    milk

--- Repository Checkpoint 10/calories.py
+++ Repository Checkpoint 10/calories.py
@@ -2,29 +2,39 @@ def main():
     print("🥛 🍉 " * 10)
     print("Count your calories!!!")
     dic = {
-    "Whole milk" : "73",
-    "Soymilk, light" : "30",
-    "Yogurt Plain" : "68.5",
-    "Yogurt Sour Cream" : "16",
-    "egg" : "75",
-    "egg white" : "16",
-    "Cream Cheese" : "17",
-    "Butter, regular, salted " : "36",
-    "Almonds" : "170",
-    "Peanuts" : "166",
-    "Black beans" : "113",
-    "Pinto beans" : "122",
-    "Chicken Wing" : "80",
-    "Pork" : "178",
-    "Carrots" : "13",
-    "Lettuce" : "2",
-    "Watermellon" : "11",
-    "Banana" : "33"
+    "Whole milk" : 73,
+    "Soymilk" : 30,
+    "Yogurt" : 68.5,
+    "Yogurt Sour Cream" : 16,
+    "egg" : 75,
+    "egg white" : 16,
+    "Cream Cheese" : 17,
+    "Butter regular and salted " : 36,
+    "Almonds" : 170,
+    "Peanuts" : 166,
+    "Black beans" : 113,
+    "Pinto beans" : 122,
+    "Chicken Wing" : 80,
+    "Pork" : 178,
+    "Carrots" : 13,
+    "Lettuce" : 2,
+    "Watermellon" : 11,
+    "Banana" : 33
     }
 
-    x = input("Write a food item from the list: ")
-    y = input("Write another food item from the list: ")
     print(dic)
+    x = str(input("Write a food item from the list: "))
+    y = str(input("Write another food item from the list: "))
+    list = [x,y]
 
+    if x == "Whole milk" or x == "Soymilk" and y == "Watermellon":
+        print ("❌Error❌: you cannot mix watermelon 🍉 and milk 🥛")
+    elif x == "Watermellon" and y == "Whole milk" or y == "Soymilk":
+        print()
+    else:
+        sum = 0
+        for value in list:
+            sum += dic[value]
+        print("the sum of", x, "and", y, "is", sum)
     
 main()

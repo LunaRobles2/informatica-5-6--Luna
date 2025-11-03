1f70e68805f15b752b9115d095f300417f47384f
LunaRobles2 <1lunabisakura@gmail.com>
2025-11-03 19:39:47 +0000

    cow

---
+++ Repository Checkpoint 11/libraries.py
@@ -0,0 +1,21 @@
+# import random
+
+#coin = random.choice(["heads", "tales"])
+#print("coin")
+
+#number = random.randint(1,10)
+#print(number)
+
+#cards = ["jack", "queen", "king", "Ace"]
+#random.shuffle(cards)
+#for card in cards:
+#     print(card)
+
+import statistics
+print(statistics.mean[100,90])
+
+import sys
+import cowsay
+
+cowsay.cow
+

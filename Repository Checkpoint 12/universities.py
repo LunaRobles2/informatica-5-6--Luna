0889b9786784e9c2a3e674b8777c11921893e584
LunaRobles2 <1lunabisakura@gmail.com>
2025-11-11 16:40:10 +0000

    carreras

--- Repository Checkpoint 12/universities.py
+++ Repository Checkpoint 12/universities.py
@@ -1,31 +1,32 @@
 def main():
     uni_list = {
         "UPN":{
-        "careers" : "Pedagogía, Psicología Educativa, Sociología de la Educación, y Educación Indígena",
+        "careers" : "4",
         "cost" : "Inscripción/Reinscripción semestral: Aproximadamente entre $715.00 y $3,300.00 pesos mexicanos, dependiendo del año y la modalidad (presencial o en línea).",
         "nearest one" : "26km"
     }, 
         "URN":{
-        "careers" : "Pedagogía, Psicología Educativa, Sociología de la Educación, y Educación Indígena",
-        "cost" : "Inscripción/Reinscripción semestral: Aproximadamente entre $715.00 y $3,300.00 pesos mexicanos, dependiendo del año y la modalidad (presencial o en línea).",
-        "nearest one" : "26km"
+        "careers" : "7",
+        "cost" : "$3,600.00 MXN al mes aproximadamente",
+        "nearest one" : "25km"
     }, 
         "UACJ":{
-        "careers" : "Pedagogía, Psicología Educativa, Sociología de la Educación, y Educación Indígena",
-        "cost" : "Inscripción/Reinscripción semestral: Aproximadamente entre $715.00 y $3,300.00 pesos mexicanos, dependiendo del año y la modalidad (presencial o en línea).",
-        "nearest one" : "26km"
+        "careers" : "8",
+        "cost" : "$5,200 pesos mexicanos aproximadamenbte.",
+        "nearest one" : "35km"
     }, 
         "elpac":{
-        "careers" : "Pedagogía, Psicología Educativa, Sociología de la Educación, y Educación Indígena",
-        "cost" : "Inscripción/Reinscripción semestral: Aproximadamente entre $715.00 y $3,300.00 pesos mexicanos, dependiendo del año y la modalidad (presencial o en línea).",
-        "nearest one" : "26km"
+        "careers" : "1",
+        "cost" : " de $4,300 pesos de inscripción por semestre y $3,500 pesos de colegiatura mensual.",
+        "nearest one" : "318km"
     }, 
-        "IASE":{
-        "careers" : "Pedagogía, Psicología Educativa, Sociología de la Educación, y Educación Indígena",
-        "cost" : "Inscripción/Reinscripción semestral: Aproximadamente entre $715.00 y $3,300.00 pesos mexicanos, dependiendo del año y la modalidad (presencial o en línea).",
-        "nearest one" : "26km"
+        "ISAC":{
+        "careers" : "2",
+        "cost" : "aproximado por semestre de $100,000 MXN en colegiatura.",
+        "nearest one" : "310km"
     }
 
 }
-#"UACJ", "EAC", "Tecmilenio", "elpac", "URN", "BYU pathway", "Tecnologico", "ICATECH", "Tec de Monterrey"
+
+    print(uni_list)
 main()

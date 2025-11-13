3a7a9990a6d73e53373f71adee43e4b494953799
LunaRobles2 <1lunabisakura@gmail.com>
2025-11-13 16:26:53 +0000

    coin

--- Repository Checkpoint 12/bitcoin.py
+++ Repository Checkpoint 12/bitcoin.py
@@ -1,4 +1,9 @@
 import requests
+print(("⬛")*25)
 API_KEY = "27be317712d2e0375eddb213e8dc4cf2fbbbb05662302b9b819757b6f9cb4e8b"
 bitcoin = float(input("How many bitcoins did you want to buy? "))
 response = requests.get ("https://rest.coincap.io/v3/assets/bitcoin?apiKey="+API_KEY)
+bitcon_price = float(response.json()["data"]["priceUsd"])
+total = round(bitcoin * bitcon_price, 2)
+print(f"Your total will be: 🪙  🪙  🪙  ${total:,.2f}🪙  🪙  🪙")
+print(("⬛")*25)

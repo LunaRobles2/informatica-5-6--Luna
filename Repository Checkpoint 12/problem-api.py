ac6602215c15e654b2d08a06c3003f0a68aa2df0
LunaRobles2 <1lunabisakura@gmail.com>
2025-11-13 20:21:25 +0000

    prod

--- Repository Checkpoint 12/problem-api.py
+++ Repository Checkpoint 12/problem-api.py
@@ -1,9 +1,26 @@
 import requests
 import json
-print(("🩷 ")*25)
+print(("🧡 🩷  ")*9)
 
 product = (input("What type of product are you searching for? "))
-response = requests.get ("http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline")
-look = float(response.json()["name"]["product_type"]["description"])
-print(f"Your product is: {look}")
-print(("⬛")*25)
+free_list = [
+"Canadian","CertClean","Chemical Free","Dairy Free","EWG Verified","EcoCert","Fair Trade","Gluten Free"
+"Hypoallergenic"
+Natural
+No Talc
+Non-GMO
+Organic
+Peanut Free Product
+Sugar Free
+USDA Organic
+Vegan
+alcohol free
+cruelty free
+oil free
+purpicks
+silicone free
+water free
+]
+free = (input("What type of free of are you searching for? "))
+response = requests.get ("http://makeup-api.herokuapp.com/api/v1/products.json?product_type="+free+product+"maybelline")
+look = (response.json()["name"]["product_type"]["description"])

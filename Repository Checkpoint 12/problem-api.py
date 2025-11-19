8ac0e706155842236f93594c1f8040e626efc4cc
LunaRobles2 <1lunabisakura@gmail.com>
2025-11-19 19:40:16 +0000

    api

--- Repository Checkpoint 12/problem-api.py
+++ Repository Checkpoint 12/problem-api.py
@@ -3,16 +3,17 @@ def main():
     
     print(("🧡 🩷  ")*10)
     print( )
-    product_list = [
-    "blush", "lip_liner","lipstick","foundation","eyeliner","eyeshadow","bronzer","mascara"
-]
-    print("💋💄",product_list)
-    product = (input("What type of product are you searching for? "))
-    tag_list = [
-    "Canadian","CertClean","Chemical Free","Dairy Free","EWG Verified","EcoCert","Fair Trade","Gluten Free","Hypoallergenic",
-    "Natural","No Talc","Non-GMO","Organic","Peanut Free Product","Sugar Free","USDA Organic","Vegan","alcohol free","cruelty free",
-    "oil free","purpicks","silicone free","water free"
-]
+    product_list = ["blush", "lip_liner","lipstick","foundation","eyeliner","eyeshadow","bronzer","mascara"]
+    tag_list = ["Canadian","CertClean","Chemical Free","Dairy Free","EWG Verified","EcoCert","Fair Trade","Gluten Free","Hypoallergenic","Natural","No Talc","Non-GMO","Organic","Peanut Free Product","Sugar Free","USDA Organic","Vegan","alcohol free","cruelty free","oil free","purpicks","silicone free","water free"]
+    try:
+        print("💋💄",product_list)
+        product = (input("What type of product are you searching for? "))
+        
+    except ValueError:
+            print("You have to write a product that is in the list")
+            return product
+
+
     print("💋💄",tag_list)
     tag = (input("What type of free of are you searching for? "))
     response = requests.get ("http://makeup-api.herokuapp.com/api/v1/products.json?product_type="+product+ "&tag_list=" +tag +"&brand=maybelline")
